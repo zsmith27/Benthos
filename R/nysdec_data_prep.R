4f67c4d429427760bfe5c3ad4fa03e6fd195186b
zsmith27 <zsmith@icprb.org>
2017-02-06 13:43:51 -0500

    Added a data prep function specific to the NYSDEC.

--- R/nysdec_data_prep.R
+++ R/nysdec_data_prep.R
@@ -1,89 +1,95 @@
 #==============================================================================
 #'Prepare the NYSDEC SBU Data
 #'
-#'@param Taxa.df = A data frame containing raw taxonomic counts.
+#'@param taxa.df = Taxonomic counts in a long data format.
+#'@param master.df = A Master Taxa List containing the appropriate and applicable
+#'taxonomic hierarchy and taxonomic traits. Specify 'master' to use the Master
+#'Taxa List built in to the Benthos package.
 #'@return Prepare taxonomic data from the New York State Department of 
 #'Environmental Conservation (NYSDEC) Stream Biomonitoring Unit (SBU).
 #'@export
 
-nysdec_data_prep <- function(Taxa.df){
+nysdec_data_prep <- function(taxa.df, master.df){
   # Column names to uppercase.
-  names(Taxa.df) <- toupper(names(Taxa.df))
+  names(taxa.df) <- toupper(names(taxa.df))
   # Check for an AGENCY_CODE column and add one if missing.
-  if(!"AGENCY_CODE" %in% names(Taxa.df)) {
-    Taxa.df$AGENCY_CODE <- "NYSDEC_SBU"
+  if(!"AGENCY_CODE" %in% names(taxa.df)) {
+    taxa.df$AGENCY_CODE <- "NYSDEC_SBU"
   } 
   # Check for a REPLICATE column. If absent, create a SAMPLE_NUMBER column
   # and fill with ones. If present, change the column name to SAMPLE_NUMBER.
-  if(!"REPLICATE" %in% names(Taxa.df)){
-    Taxa.df$SAMPLE_NUMBER <- 1
+  if(!"REPLICATE" %in% names(taxa.df)){
+    taxa.df$SAMPLE_NUMBER <- 1
   } else {
-    names(Taxa.df)[names(Taxa.df) %in% "REPLICATE"] <- "SAMPLE_NUMBER"
-    Taxa.df$SAMPLE_NUMBER[is.na(Taxa.df$SAMPLE_NUMBER)] <- 1
+    names(taxa.df)[names(taxa.df) %in% "REPLICATE"] <- "SAMPLE_NUMBER"
+    taxa.df$SAMPLE_NUMBER[is.na(taxa.df$SAMPLE_NUMBER)] <- 1
   }
   # Change column name COLL_DATE to DATE.
-  names(Taxa.df)[names(Taxa.df) %in% "COLL_DATE"] <- "DATE"
+  names(taxa.df)[names(taxa.df) %in% "COLL_DATE"] <- "DATE"
   # Change column name SITE_ID to STATION_ID.
-  names(Taxa.df)[names(Taxa.df) %in% "SITE_ID"] <- "STATION_ID"
+  names(taxa.df)[names(taxa.df) %in% "SITE_ID"] <- "STATION_ID"
   # Make sure the STATION_ID column is numeric.
-  Taxa.df$STATION_ID <- as.character(Taxa.df$STATION_ID)
+  taxa.df$STATION_ID <- as.character(taxa.df$STATION_ID)
   # Column GENSPECIES to uppercase.
-  Taxa.df$GENSPECIES <- toupper(as.character(Taxa.df$GENSPECIES))
+  taxa.df$GENSPECIES <- toupper(as.character(taxa.df$GENSPECIES))
   # Change column name INDIV to REPORTING_VALUE.
-  names(Taxa.df)[names(Taxa.df) %in% "INDIV"] <- "REPORTING_VALUE"
-  Taxa.df$REPORTING_VALUE <- as.numeric(as.character(Taxa.df$REPORTING_VALUE))
+  names(taxa.df)[names(taxa.df) %in% "INDIV"] <- "REPORTING_VALUE"
+  taxa.df$REPORTING_VALUE <- as.numeric(as.character(taxa.df$REPORTING_VALUE))
   # Check for EVENT_ID column. If absent, create one.
-  if(!"EVENT_ID" %in% names(Taxa.df)) {
-    Taxa.df$EVENT_ID <- with(Taxa.df, paste(STATION_ID, DATE, SAMPLE_NUMBER, sep = "_"))
+  if(!"EVENT_ID" %in% names(taxa.df)) {
+    taxa.df$EVENT_ID <- with(taxa.df, paste(STATION_ID, DATE, SAMPLE_NUMBER, sep = "_"))
   }
   
   
   
-  final.df <- Taxa.df[, c("EVENT_ID", "STATION_ID", "AGENCY_CODE", "DATE",
+  agency.df <- taxa.df[, c("EVENT_ID", "STATION_ID", "AGENCY_CODE", "DATE",
                           "SAMPLE_NUMBER", "GENSPECIES", "REPORTING_VALUE")]
-  final.df$GENUS <- final.df$GENSPECIES
+  agency.df$GENUS <- agency.df$GENSPECIES
   # Remove any UNDETERMINED
-  final.df$GENUS <- gsub("UNDETERMINED ","", final.df$GENUS)
+  agency.df$GENUS <- gsub("UNDETERMINED ","", agency.df$GENUS)
   # Remove any text contained within parentheses
-  final.df$GENUS <- gsub("\\([^\\)]+\\)","", final.df$GENUS)
+  agency.df$GENUS <- gsub("\\([^\\)]+\\)","", agency.df$GENUS)
   # Remove NR.
-  final.df$GENUS <- gsub("\ NR\\.","", final.df$GENUS)
+  agency.df$GENUS <- gsub("\ NR\\.","", agency.df$GENUS)
   # Remove GR.
-  final.df$GENUS <- gsub("\ GR\\.","", final.df$GENUS)
+  agency.df$GENUS <- gsub("\ GR\\.","", agency.df$GENUS)
   # Remove ?
-  final.df$GENUS <- gsub("\\?","", final.df$GENUS)
-  final.df$GENUS <- trimws(final.df$GENUS)
-  final.df$GENUS <- ifelse(grepl(" ",  final.df$GENUS),
-                           gsub( " .*$", "", final.df$GENUS),
-                           final.df$GENUS)
+  agency.df$GENUS <- gsub("\\?","", agency.df$GENUS)
+  agency.df$GENUS <- trimws(agency.df$GENUS)
+  agency.df$GENUS <- ifelse(grepl(" ",  agency.df$GENUS),
+                           gsub( " .*$", "", agency.df$GENUS),
+                           agency.df$GENUS)
   # Complexes are not allowed. Pick the first taxon listed in the complex.
-  final.df$GENUS <- ifelse(grepl("/",  final.df$GENUS),
-                           gsub( "/.*$", "", final.df$GENUS),
-                           final.df$GENUS)
+  agency.df$GENUS <- ifelse(grepl("/",  agency.df$GENUS),
+                           gsub( "/.*$", "", agency.df$GENUS),
+                           agency.df$GENUS)
   
   # Remove all genera, groups, undetermineds, complexes, and uncertainties
-  final.df$FINAL_ID <- sapply(final.df$GENSPECIES, function(x){
+  agency.df$FINAL_ID <- sapply(agency.df$GENSPECIES, function(x){
     remove <- c("SP\\.", "SPP\\.", "CF\\.", "UNDET\\.", "UNDETERMINED", "/")
     ifelse(grepl(paste(remove, collapse="|"), x), "", paste(x))
   })
   # Remove any text contained within parentheses
-  final.df$FINAL_ID <- gsub("\\([^\\)]+\\)","", final.df$FINAL_ID)
+  agency.df$FINAL_ID <- gsub("\\([^\\)]+\\)","", agency.df$FINAL_ID)
   # Remove NR.
-  final.df$FINAL_ID <- gsub("\ NR\\.","", final.df$FINAL_ID)
+  agency.df$FINAL_ID <- gsub("\ NR\\.","", agency.df$FINAL_ID)
   # Remove GR.
-  final.df$FINAL_ID <- gsub("\ GR\\.","", final.df$FINAL_ID)
+  agency.df$FINAL_ID <- gsub("\ GR\\.","", agency.df$FINAL_ID)
   # Remove ?
-  final.df$FINAL_ID <- gsub("\\?","", final.df$FINAL_ID)
+  agency.df$FINAL_ID <- gsub("\\?","", agency.df$FINAL_ID)
   # Replace the space between genus and FINAL_ID with "_"
-  final.df$FINAL_ID <- gsub(" ","_", final.df$FINAL_ID)
+  agency.df$FINAL_ID <- gsub(" ","_", agency.df$FINAL_ID)
   # Replace the blanks with "UNDETERMINED"
-  #final.df$FINAL_ID <- sub("^$", "UNDETERMINED", final.df$FINAL_ID)
-  final.df$FINAL_ID <- sub("^$", NA, final.df$FINAL_ID)
+  #agency.df$FINAL_ID <- sub("^$", "UNDETERMINED", agency.df$FINAL_ID)
+  agency.df$FINAL_ID <- sub("^$", NA, agency.df$FINAL_ID)
   #============================================================================
-  
-  final.df[, c("GENUS", "FINAL_ID")] <- t(apply(final.df[, c("GENUS", "FINAL_ID")], 1, zoo::na.locf))
-  final.df <- final.df[c("EVENT_ID", "STATION_ID", "AGENCY_CODE", "DATE",
+  agency.df[, c("GENUS", "FINAL_ID")] <- t(apply(agency.df[, c("GENUS", "FINAL_ID")], 1, zoo::na.locf))
+  agency.df <- agency.df[c("EVENT_ID", "STATION_ID", "AGENCY_CODE", "DATE",
                          "SAMPLE_NUMBER", "FINAL_ID", "REPORTING_VALUE")]
+  #============================================================================
+  # Use the generic data_prep function to merge the taxonomic counts with the
+  # Master Taxa List (master.df).
+  final.df <- data_prep(agency.df, master.df)
   
   return(final.df)
 }
